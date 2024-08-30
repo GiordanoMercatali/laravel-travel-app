2fc82fe8aa23a65a631346cb3ad926e8205eef6a
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-08-30 11:52:07 +0200

    edit page + minor fix to create

--- resources/views/admin/travels/edit.blade.php
+++ resources/views/admin/travels/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container mt-5">
 
-        <h2 class="text-center">Create Travel</h2>
+        <h2 class="text-center">Edit Travel {{ $travel->title }}</h2>
 
 
         @if ($errors->any())
@@ -15,39 +15,40 @@
         @endif
 
 
-        <form class="mt-5" action="{{ route('admin.travels.store') }}" method="POST" enctype="multipart/form-data">
+        <form class="mt-5" action="{{ route('admin.travels.store', ['travel' => $travel->id]) }}" method="POST" enctype="multipart/form-data">
             @csrf
+            @method('PUT')
 
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control " id="title" name="title" value="{{ old('title') }}">
+                <input type="text" class="form-control " id="title" name="title" value="{{ old('title', $travel->title) }}">
             </div>
 
-            <label for="cover_image" class="form-label">Image</label>
-            <input type="file" class="mb-3 form-control" id="cover_image" name="cover_image">
+            <label for="cover-image" class="form-label">Image</label>
+            <input type="file" class="mb-3 form-control" id="cover_image" name="cover_image" value="{{ old('cover_image', $travel->cover_image) }}">
 
             <div class="mb-3">
                 <label for="content" class="form-label">Description</label>
-                <textarea class="form-control" id="description" rows="3" name="description">{{ old('description') }}</textarea>
+                <textarea class="form-control" id="descrption" rows="3" name="description">{{ old('description', $travel->description) }}</textarea>
             </div>  
             
             <div class="mb-3">
                 <label for="start_date" class="form-label">Start Date</label>
                 <input type="date" class="form-control " id="start_date" name="start_date" value="2024-06-01"
-                min="2024-06-01" max="2024-08-30">
+                min="2024-06-01" max="2024-08-30" value="{{ old('start_date', $travel->start_date) }}">
             </div>
 
             <div class="mb-3">
                 <label for="end_date" class="form-label">End Date</label>
                 <input type="date" class="form-control " id="end_date" name="end_date" value="2024-08-31"
-                min="2024-06-02" max="2024-08-31">
+                min="2024-06-02" max="2024-08-31" value="{{ old('end_date', $travel->end_date) }}">
             </div>
 
             <div class="mb-3">
                 @foreach ($stages as $stage)
                 <div class="form-check">
                     <label for="stage-{{ $stage->id }}">{{ $stage->name }}</label>
-                    <input @checked(in_array($stage->id, old('stages', []))) type="checkbox" name="stage-id" id="stage-{{ $stage->id }}" value="{{ $stage->id }}">
+                    <input @checked(in_array($stage->id, old('stages', []))) type="checkbox" id="stage-{{ $stage->id }}" value="{{ $stage->id }}" name="stages[]">
                     {{-- in_array() Checks if a value exists in an array --}}
 
                 </div>
